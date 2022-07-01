e7abfcf207372bc702a4214358b7ae7588dc7d8e
osala-eng <jashonosala@gmail.com>
2022-07-01 10:29:06 -0700

    test 3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-# define true 1
-# define false 0
 
 /**
  * main - Program entry point
@@ -11,21 +9,14 @@
 int main(void)
 {
 	int num, i, j, k;
-	int done = false;
 
 	for (num = 0; num < 10; num++)
 	{
-		if (done)
-			break;
 		for (i = 0; i < 10; i++)
 		{
-			if (done)
-				break;
 			for (j = num; j < 9; j++)
 			{
-				if (done)
-					break;
-				for (j > 0 ? (k = 0) : (k = i + 1); k < 10; k++)
+				for (k = i + 1; k < 10; k++)
 				{
 					putchar(num + '0');
 					putchar(i + '0');
@@ -33,13 +24,11 @@ int main(void)
 					putchar(j + '0');
 					putchar(k + '0');
 					if (num == 9 && i == 8 && j == 9 && k == 9)
-					{
-						done = true;
 						break;
-					}
 					putchar(',');
 					putchar(' ');
 				}
+				k = 0;
 			}
 		}
 	}
