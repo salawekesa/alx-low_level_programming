98e387b8a4b1375fa4c1274f2bbcca8455cc3dad
theebantu <38879395+theebantu@users.noreply.github.com>
2022-07-21 01:00:59 +0300

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - print a string in rev using recursion
+ * _print_rev_recursion - printing a string in rev by use o recursion
  * @s: String to print
  *
  * Return: void
