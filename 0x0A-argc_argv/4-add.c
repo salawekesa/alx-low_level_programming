c6fea037361382a2e70ba180f310c267aaf4b8a1
theebantu <38879395+theebantu@users.noreply.github.com>
2022-07-23 03:39:44 +0300

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * is_num - iterate through each argv to test if it's a number
+ * is_num - iterate each argv to test if it's a number
  * @argvv: a argv
  * Return: true only if entire string is a number, false if not
  */
