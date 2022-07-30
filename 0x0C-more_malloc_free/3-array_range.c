3314c7e7b9763211c98c3a5f223ab140be4e78b0
salawekesa <wekesa9235@gmail.com>
2022-07-29 18:06:23 -0700

    Task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * array_range - creates an array of integers.
+ * array_range - creating an array of integers.
  * @min: minimum value.
  * @max: maximum value.
  *
