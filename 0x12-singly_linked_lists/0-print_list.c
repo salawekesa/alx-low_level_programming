5dac12dd72bc7c854a71b1b2597247179eb6507b
salawekesa <107699372+salawekesa@users.noreply.github.com>
2022-08-11 22:11:08 +0300

    Add files via upload

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,6 @@
-#include "main.h"
+#include "lists.h"
 /**
- * print_list - prints the elements of a list_t list.
+ * print_list - prints all the elements of a list_t list.
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
