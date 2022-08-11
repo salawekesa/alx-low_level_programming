53902f3bc1584e330fa801718df302b0135bac8d
salawekesa <wekesa9235@gmail.com>
2022-08-11 12:19:04 -0700

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end
+ * add_node_end - adding a new node at the end
  * of a list_t list.
  * @head: head of the linked list.
  * @str: string to store in the list.
