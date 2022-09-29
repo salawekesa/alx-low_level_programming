4f3e8bd6bc92e3898a492fc2f324b1c7171987fa
salawekesa <wekesa9235@gmail.com>
2022-09-29 12:47:05 -0700

    Summing a list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *  sum_dlistint - returns the sum of all
+ *  sum_dlistint - returning the sum of all
  * the data (n) of a dlistint_t linked list.
  * @head: double pointer to the head of the list
  *
